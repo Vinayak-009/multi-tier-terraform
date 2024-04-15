98e0aad7a3bb7f865235e7ee2b4df9cc42011623
Vinayak Sharma <83303847+Vinayak-009@users.noreply.github.com>
2024-04-15 23:06:58 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "Google Cloud project ID"
+  default     = "black-beach-416616"
+}
+
+variable "region" {
+  description = "Google Cloud region"
+  default     = "us-east1"
+}
+
+variable "zone" {
+  description = "Google Cloud zone"
+  default     = "us-east1-b"
+}
